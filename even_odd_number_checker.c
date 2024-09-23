7763330ab945a61aa4e087e7e88e8d3ee45d040b
[sergio eneide] <[sergio.eneide@protonmail.com]>
2024-09-23 11:47:35 +0100

    fourth commit

--- even_odd_number_checker.c
+++ even_odd_number_checker.c
@@ -4,12 +4,20 @@ int main()
 {
     // Declare integer variable 'num'
     int num;
-    
+
     // Integer input from user
     printf("Enter a positive integer: ");
     // Read value of user integer input, store in variable 'num'
     scanf("%d", &num);
+    
+    // if-else statement checks if user integer input is even or odd
+    if (num % 2 == 0) {
+        printf("%d is even.\n", num);
+    } else {
+        printf("%d is odd.\n", num);
+    }
 
+    return 0;
 
 
 }
