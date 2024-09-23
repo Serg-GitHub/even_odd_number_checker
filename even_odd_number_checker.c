1d7d3c7ff835b975ca8ef41293dd61c1577c4b35
[sergio eneide] <[sergio.eneide@protonmail.com]>
2024-09-23 11:42:30 +0100

    third commit

--- even_odd_number_checker.c
+++ even_odd_number_checker.c
@@ -4,7 +4,11 @@ int main()
 {
     // Declare integer variable 'num'
     int num;
-
+    
+    // Integer input from user
+    printf("Enter a positive integer: ");
+    // Read value of user integer input, store in variable 'num'
+    scanf("%d", &num);
 
 
 
